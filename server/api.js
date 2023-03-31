4b10c61e85b8374abbd7b5392462af1593fdbf80
foulde <dardill.hugo@edu.devinci.fr>
2023-03-31 19:07:05 +0200

    search and id added

--- server/api.js
+++ server/api.js
@@ -51,5 +51,49 @@ app.post('/products', async (req, res) => {
   }
 });
 
+
+app.get('/products/search', async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 12;
+    const brand = req.query.brand;
+    const price = req.query.price;
+
+    // Build the query object based on provided filters
+    const query = {};
+    if (brand) query.brand_name = brand;
+    if (price) query.price = { $lte: price };
+
+    // Fetch the products using the query object, limit, and sort by price
+    const products = await Product.find(query)
+      .limit(limit)
+      .sort({ price: 1 });
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
 // Start the server
 app.listen(process.env.PORT || 3000, () => console.log(`Server started on port ${process.env.PORT || 3000}`));
