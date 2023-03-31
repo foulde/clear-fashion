3baa48f50c37317bf9bcbd92ed6f1f76a250b6b8
foulde <dardill.hugo@edu.devinci.fr>
2023-03-31 19:22:57 +0200

    change for search brands

--- server/api.js
+++ server/api.js
@@ -93,6 +93,38 @@ app.get('/products/:id', async (req, res) => {
   }
 });
 
+app.get('/brands', async (req, res) => {
+  try {
+    const brands = await Product.distinct('brand_name');
+
+    if (!brands || brands.length === 0) {
+      return res.status(404).json({ message: 'No brands found' });
+    }
+
+    res.status(200).json(brands);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+app.get('/products/brand/:brand', async (req, res) => {
+  try {
+    const { brand } = req.params;
+    const products = await Product.find({ brand_name: brand });
+
+    if (!products || products.length === 0) {
+      return res.status(404).json({ message: 'No products found for the specified brand' });
+    }
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 
 // Start the server
