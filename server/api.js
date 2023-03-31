e44700f21250d04d8041933498cc6ea8a0f9c8ee
foulde <dardill.hugo@edu.devinci.fr>
2023-03-31 17:48:18 +0200

    test

--- server/api.js
+++ server/api.js
@@ -1,106 +1,100 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
 const Product = require('./models/productModel');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-//express middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
 
-// to declare routes
+// Routes
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.send('Hello, world!');
 });
 
-//j'ai fait app.get pour tester
-app.get('/brand', (req, res) => {
-  res.send('there are some brands here');
+// Create a new product
+app.post('/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-//to get data from database
+// Retrieve all products
 app.get('/products', async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-// to get a specific product by its id
+// Retrieve a single product by ID
 app.get('/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.status(200).json(product);
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-app.post('/products', async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//update a product
+// Update a product by ID
 app.put('/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `Cannot find product with ID ${id}` });
+    const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ error: 'Product not found' });
     }
-    const updatedProduct = await Product.findByIdAndUpdate(
-      id,
-      req.body,
-      { new: true }
-    );
-    res.status(200).json(updatedProduct);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-// delete product
+// Delete a product by ID
 app.delete('/products/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `Cannot find product with ID ${id}` });
+    if (product) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ error: 'Product not found' });
     }
-    res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-mongoose
-  .connect('mongodb+srv://hugodardill:hugodardill@cluster0.meat52d.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+// Connect to MongoDB and start the server
+mongoose.connect('mongodb+srv://hugodardill:hugodardill@cluster0.uaokru6.mongodb.net/?retryWrites=true&w=majority/products', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Node API is running on port ${PORT}`);
-    });
     console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
   })
   .catch((error) => {
-    console.log(error);
+    console.error(error);
   });
-
-mongoose.set('strictQuery', false);
